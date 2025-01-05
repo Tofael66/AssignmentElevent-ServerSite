fba08a1df80c1f58debec50311082b06766bd212
Tofael66 <tofael474@gmail.com>
2025-01-05 23:59:24 +0600

    delete section

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ app.use(express.json())
  * some setion complate
  * some setion complate
  * some setion complate
- * some setion complate
- * some setion complate
- * some setion complate
+
  */
 
 
